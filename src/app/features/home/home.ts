f7f0fe3e12461a070960892add8847123b9db10d
Denyson Cardoso <denyson.cardoso@hotmail.com>
2025-09-07 12:10:09 -0300

    adicionando componente "no-transactions", implementando lógica para exibir mensagem quando não há transações e integrando ao componente "home"

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,13 +3,15 @@ import {Balance} from './components/balance/balance';
 import {TransactionItem} from './components/transaction-item/transaction-item';
 import {Transaction} from '../../shared/transactions/interfaces/transaction';
 import {TransactionType} from '../../shared/transactions/enums/transactions-types';
+import {NoTransactions} from './components/no-transactions/no-transactions';
 
 
 @Component({
   selector: 'app-home',
   imports: [
     Balance,
-    TransactionItem
+    TransactionItem,
+    NoTransactions
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
@@ -17,9 +19,9 @@ import {TransactionType} from '../../shared/transactions/enums/transactions-type
 export class Home {
 
   transactions = signal<Transaction[]>([
-    { title: 'Salário', value: 1000, type: TransactionType.income },
-    { title: 'Aluguel', value: 500, type: TransactionType.outcome },
-    { title: 'Comida', value: 500, type: TransactionType.outcome},
+    // { title: 'Salário', value: 1000, type: TransactionType.income },
+    // { title: 'Aluguel', value: 500, type: TransactionType.outcome },
+    // { title: 'Comida', value: 500, type: TransactionType.outcome},
 
   ]);
 
