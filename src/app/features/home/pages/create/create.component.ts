f07ff5585141017ad656c2922397cfa7254830e2
Denyson Cardoso <denyson.cardoso@hotmail.com>
2025-09-10 19:37:55 -0300

    adicionando formulário reativo no componente "create" com campo de título obrigatório e validação básica

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  form = new FormGroup({
+
+    title: new FormControl('', {
+      validators: [Validators.required]
+    }),
+
+  })
 
 }
