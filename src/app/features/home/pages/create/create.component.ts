8dfafef905fc86d6aad3294481fafffdb957ebca
Denyson Cardoso <denyson.cardoso@hotmail.com>
2025-10-25 18:44:03 -0300

    adicionando suporte à biblioteca "ngx-mask" para formatação de valores no formulário do componente "create" e incluindo novos campos "valor" e "tipo" com validações adequadas

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,7 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+
+import {Transaction} from '../../../../shared/transactions/interfaces/transaction';
+import {TransactionType} from '../../../../shared/transactions/enums/transactions-types';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {NgxMaskDirective} from 'ngx-mask';
 
 
 @Component({
@@ -9,18 +16,30 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    JsonPipe,
+    MatButtonModule,
+    MatButtonToggleModule,
+    NgxMaskDirective,
   ],
   templateUrl: './create.component.html',
+  standalone: true,
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  transaction = input<Transaction>();
+
+  readonly transationType = TransactionType;
 
   form = new FormGroup({
 
     title: new FormControl('', {
       validators: [Validators.required]
     }),
+    value: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    type: new FormControl('', {})
 
   })
 
